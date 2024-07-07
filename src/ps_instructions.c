2be3ca5dcfb8e67eaa53b574543aa6854b65119a
fireframes <82419815+fireframes@users.noreply.github.com>
2024-07-07 17:55:46 +0700

    Revert "instructions done"
    This reverts commit 58ab4ff50fb5e6ef783b56037964124683fb511b.

--- src/ps_instructions.c
+++ src/ps_instructions.c
@@ -6,22 +6,22 @@
 /*   By: mmaksimo <mmaksimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 17:01:52 by mmaksimo          #+#    #+#             */
-/*   Updated: 2024/07/07 17:04:09 by mmaksimo         ###   ########.fr       */
+/*   Updated: 2024/07/05 22:25:08 by mmaksimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 
-/*	sa : Swap the first 2 elements at the top of stack A.
-	
-	Do nothing if there is only one or no elements.*/
+/*	Swap the first 2 elements at the top of stack a.
+	#	Do nothing if there is only one or no elements.*/
 void	swap_a(t_stack **stack_a)
 {
 	t_stack	*node1;
 	t_stack	*node2;
 
 	node1 = *stack_a;
+	/* return if there is only 1 or 0 nodes -- can check in earlier calls*/
 	if (!node1 || node1->next == NULL)
 		return ;
 	node2 = node1->next;
@@ -33,15 +33,14 @@ void	swap_a(t_stack **stack_a)
 	write(1, "sa\n", 3);
 }
 
-/*	sb : Swap the first 2 elements at the top of stack B.
-	
-	Do nothing if there is only one or no elements.*/
 void	swap_b(t_stack **stack_b)
 {
 	t_stack	*node1;
 	t_stack	*node2;
 
+
 	node1 = *stack_b;
+	/* return if there is only 1 or 0 nodes -- can check in earlier calls*/
 	if (!node1 || node1->next == NULL)
 		return ;
 	node2 = node1->next;
@@ -53,141 +52,44 @@ void	swap_b(t_stack **stack_b)
 	write(1, "sb\n", 3);
 }
 
-/*	ss : sa and sb at the same time.*/
+/*	sa and sb at the same time */
 void	swap_ss(t_stack **stack_a, t_stack **stack_b)
 {
 	swap_a(stack_a);
 	swap_b(stack_b);
 }
 
-/*	pa : Take the first element at the top of B and put it at the top of A.
-
-	Do nothing if B is empty.*/
+/*	Take the first element at the top of B and put it at the top of A.
+	#	Do nothing if B is empty.*/
 void	push_a(t_stack **stack_a, t_stack **stack_b)
 {
-	t_stack	*push_node;
-	t_stack	*new_head_b;
+	t_stack *push_node;
+	t_stack *new_head_b;
 
 	if (*stack_b == NULL)
 		return ;
 	push_node = *stack_b;
-	if (push_node->next != NULL)
-	{
-		new_head_b = push_node->next;
-		new_head_b->prev = NULL;
-		*stack_b = new_head_b;
-	}
-	else
-		*stack_b = NULL;
+	new_head_b = push_node->next;
+	new_head_b->prev = NULL;
+	*stack_b = new_head_b;
 	ps_dbllstadd_front(stack_a, push_node);
-	write(1, "pa\n", 3);
 }
 
-/*	pb : Take the first element at the top of A and put it at the top of B.
-
-	Do nothing if A is empty.*/
+/*	Take the first element at the top of A and put it at the top of B.
+	#	Do nothing if A is empty.*/
 void	push_b(t_stack **stack_a, t_stack **stack_b)
 {
-	t_stack	*push_node;
-	t_stack	*new_head_a;
+	t_stack *push_node;
+	t_stack *new_head_a;
 
 	if (*stack_a == NULL)
 		return ;
 	push_node = *stack_a;
-	if (push_node->next != NULL)
-	{
-		new_head_a = push_node->next;
-		new_head_a->prev = NULL;
-		*stack_a = new_head_a;
-	}
-	else
-		*stack_a = NULL;
+	new_head_a = push_node->next;
+	new_head_a->prev = NULL;
+	*stack_a = new_head_a;
 	ps_dbllstadd_front(stack_b, push_node);
-	write(1, "pb\n", 3);
 }
 
-/*	ra : Shift up all elements of stack A by 1.
 
-	The first element becomes the last one.*/
-void	rotate_a(t_stack **stack_a)
-{
-	t_stack	*first;
-	t_stack	*second;
 
-	if (*stack_a == NULL)
-		return ;
-	first = *stack_a;
-	second = first->next;
-	ps_dbllstadd_back(stack_a, first);
-	*stack_a = second;
-	write(1, "ra\n", 3);
-}
-
-/*	rb : Shift up all elements of stack B by 1.
-
-	The first element becomes the last one.*/
-void	rotate_b(t_stack **stack_b)
-{
-	t_stack	*first;
-	t_stack	*second;
-
-	if (*stack_b == NULL)
-		return ;
-	first = *stack_b;
-	second = first->next;
-	ps_dbllstadd_back(stack_b, first);
-	*stack_b = second;
-	write(1, "rb\n", 3);
-}
-
-/*	rr : ra and rb at the same time.*/
-void	rotate_rr(t_stack **stack_a, t_stack **stack_b)
-{
-	rotate_a(stack_a);
-	rotate_b(stack_b);
-}
-
-/*	rra : Shift down all elements of stack A by 1.
-
-	The last element becomes the first one.*/
-void	rev_rotate_a(t_stack **stack_a)
-{
-		t_stack *last;
-		t_stack *new_last;
-
-		if (*stack_a == NULL)
-			return ;
-		last = *stack_a;
-		while (last->next != NULL)
-			last = last->next;
-		new_last = last->prev;
-		new_last->next = NULL;
-		ps_dbllstadd_front(stack_a, last);
-		write(1, "rra\n", 4);
-}
-
-/*	rrb : Shift down all elements of stack B by 1.
-
-	The last element becomes the first one.*/
-void	rev_rotate_b(t_stack **stack_b)
-{
-	t_stack *last;
-	t_stack *new_last;
-
-	if (*stack_b == NULL)
-		return ;
-	last = *stack_b;
-	while (last->next != NULL)
-		last = last->next;
-	new_last = last->prev;
-	new_last->next = NULL;
-	ps_dbllstadd_front(stack_b, last);
-	write(1, "rrb\n", 4);
-}
-
-/*	rrr : rra and rrb at the same time.*/
-void	rev_rotate_rrr(t_stack **stack_a, t_stack **stack_b)
-{
-	rev_rotate_a(stack_a);
-	rev_rotate_b(stack_b);
-}
--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mmaksimo <mmaksimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 20:17:55 by mmaksimo          #+#    #+#             */
-/*   Updated: 2024/07/07 17:41:02 by mmaksimo         ###   ########.fr       */
+/*   Updated: 2024/07/05 22:25:23 by mmaksimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@ int	main(int argc, char **argv)
 
 
 	stack_a	= NULL;
-	stack_b	= NULL; //ps_dbllstnew(42);
-	// ps_dbllstadd_back(&stack_b, ps_dbllstnew(21));
+	stack_b	= ps_dbllstnew(42);
+	ps_dbllstadd_back(&stack_b, ps_dbllstnew(21));
 
 	int	i;
 
@@ -39,74 +39,58 @@ int	main(int argc, char **argv)
 		i++;
 	}
 	// ft_printf("head data: %d\n", stack_a->data);
-
-	// push_b(&stack_a, &stack_b);
-	// push_b(&stack_a, &stack_b);
-	// push_b(&stack_a, &stack_b);
-	// push_b(&stack_a, &stack_b);
-	// push_b(&stack_a, &stack_b);
-	// push_b(&stack_a, &stack_b);
-	// push_b(&stack_a, &stack_b);
-
-
 	// swap_a(&stack_a);
 	// swap_b(&stack_b);
 	// swap_ss(&stack_a, &stack_b);
-	
-	// rev_rotate_a(&stack_a);
-	// rotate_a(&stack_a);
-	// swap_ss(&stack_a, &stack_b);
-	// push_a(&stack_a, &stack_b);
-	// push_a(&stack_a, &stack_b);
+	push_a(&stack_a, &stack_b);
 
-	// rev_rotate_b(&stack_a);
-	// rotate_b(&stack_a);
-	// swap_ss(&stack_a, &stack_b);
-
-	// rev_rotate_rrr(&stack_a, &stack_b);
-
-	// rotate_rr(&stack_a, &stack_b);
-
-	if (stack_a)
-		ft_printf("head a: %d\n", stack_a->data);
-	else
-		ft_printf("head a: (null)\n");
-
-	if (stack_b)
-		ft_printf("head b: %d\n", stack_b->data);
-	else
-		ft_printf("head b: (null)\n");
-	
-	ft_printf("\u2014\t\u2014\n\n");
-
+	ft_printf("--\n");
 
 	// Print lists
 	t_stack *demo_a = stack_a;
-	t_stack *demo_b = stack_b;
-
-	while (demo_a || demo_b)
+	while (demo_a != NULL)
 	{
-		if (demo_a != NULL)
-		{
-			ft_printf("%d\t", demo_a->data);
-			demo_a = demo_a->next;
-		}
-		else
-			ft_printf(" \t");
-		if (demo_b != NULL)
-		{
-			ft_printf("%d", demo_b->data);
-			demo_b = demo_b->next;
-		}
-		else
-			ft_printf(" \t");
-		ft_printf("\n");
+		ft_printf("%d\n", demo_a->data);
+
+		demo_a = demo_a->next;
 	}
-	ft_printf("\u2014\t\u2014\n");
+	ft_printf("_\na\n");
+	ft_printf("\nb:\n\n");
 
-	ft_printf("a\tb\n");
+	t_stack *demo_b = stack_b;
+	while (demo_b != NULL)
+	{
+		ft_printf("%d\n", demo_b->data);
 
-	// ft_printf("head data: %d\n", stack_b->data);
+		demo_b = demo_b->next;
+	}
+	ft_printf("head data: %d\n", stack_b->data);
+
+
+
+	ft_printf("--\n");ft_printf("--\n");
+	push_b(&stack_a, &stack_b);
+
+
+	// Print lists after push_b
+	t_stack *demo2_a = stack_a;
+	while (demo2_a != NULL)
+	{
+		ft_printf("%d\n", demo2_a->data);
+
+		demo2_a = demo2_a->next;
+	}
+	ft_printf("_\na\n");
+	ft_printf("\nb:\n\n");
+
+	t_stack *demo2_b = stack_b;
+	while (demo2_b != NULL)
+	{
+		ft_printf("%d\n", demo2_b->data);
+
+		demo2_b = demo2_b->next;
+	}
+	ft_printf("head data: %d\n", stack_b->data);
 
 	ps_dbllstclear(&stack_a);
 	ps_dbllstclear(&stack_b);
